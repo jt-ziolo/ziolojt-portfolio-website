b5748f21113b597d289535bced64d8ed2b83f8d5
JT Ziolo <ziolojt@gmail.com>
2024-03-18 14:00:02 -0600

    feat: add site info ts file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import BackgroundImage from "@/components/background-image";
 
---
+++ src/site-info.ts
@@ -0,0 +1,13 @@
+type Info = {
+  topLevelDomain: string;
+  titleTemplate: string;
+  titleDefault: string;
+  description: string;
+};
+
+export const siteInfo: Info = {
+  topLevelDomain: "https://ziolojt.com",
+  titleTemplate: "%s | JT Ziolo",
+  titleDefault: "JT Ziolo — Full Stack Web Development",
+  description: "...",
+};
