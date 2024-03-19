57527e18eb641f1809815c4cc87c7efb3876f0a4
JT Ziolo <ziolojt@gmail.com>
2024-03-18 18:23:39 -0600

    feat: add sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,18 +11,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${topLevelDomain}/about`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
-    },
-    {
-      url: `${topLevelDomain}/portfolio`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
       url: `${topLevelDomain}/resume.pdf`,
       lastModified: new Date(),
       changeFrequency: "monthly",
